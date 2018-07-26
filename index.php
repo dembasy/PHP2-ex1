670f4e53ba8599b5b1fffabe90311dc36285791e
dembasy <dembasy1227@gmail.com>
2018-07-26 15:17:19 +0200

    done

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>exercice1</title>
+  </head>
+  <body>
+    <p><?php $age = 23;
+    if ($age => 18 ) {
+      echo 'Vous êtes majeur'
+          }  ?></p>
+  </body>
+</html>
